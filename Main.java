636687bb438e3fc9d1e8910a74d85f9650060514
Jilani Rj <jilanirj16027@gmail.com>
2024-03-07 11:54:06 +0530

    jenkins

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Java");
+        System.out.println("This is the first program");
+        System.out.println(new Date());
+    }
+}
